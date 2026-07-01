6a81329fc9c05b18c3471d17a731ebcd0501e001
nishlfernando11 <nlfernando11@gmail.com>
2026-07-01 16:29:04 +1000

    add git deployment files

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
     output: 'export',
     reactStrictMode: true,
     // Replace 'your-repo-name' with your exact GitHub repository name
-    basePath: 'nishlfernando11/Port4lio', 
-    assetPrefix: 'nishlfernando11/Port4lio/', 
+    basePath: '/nishlfernando11/Port4lio', 
+    assetPrefix: '/nishlfernando11/Port4lio/', 
 };
 
 module.exports = nextConfig;
